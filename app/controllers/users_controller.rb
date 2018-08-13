3a2ee04cd44f440f5dacfa33f906ac5739ceb18f
tomokiyao <tomoki9226@gmail.com>
2018-08-13 10:33:06 +0000

    user_profile 途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,14 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user =User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:name,:kind_id,:residence,:profile_image,:pr,:experience)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ad0e31d2b16d89a4a35dd81cdd30f8546c199788e27f94947fd6cb5ccd915c36e0649a9e9746db5f4a7d1ccfc90f5ac2491c22938ad666dffa16eabbf8ff2ee8'
